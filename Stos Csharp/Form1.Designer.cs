54b90df1371bbf9e5f6de6d13561f4942eb79ff0
Delta Ink Design <izula97@gmail.com>
2019-01-08 22:57:09 +0100

    Dodaj pliki projektów.

---
+++ Stos Csharp/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Stos_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PUSH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "POP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "CLEAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(138, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(211, 442);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(369, 487);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Stack Visualization";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Stos Csharp/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Stos_Csharp
+{
+
+    public partial class Form1 : Form
+    {
+        Stack<TextBox> st = new Stack<TextBox>(10);
+        //List<int> numbers = new List<int>();
+        int[] tablicaY;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tablicaY = new int[10]; //tablica koordynatow wysokosci
+
+            for(int i = 0; i <10; i++) //oblicza wysokosc dla danego textboxa
+            {
+
+                tablicaY[i]= 380 + 20 * (-i * 2);
+            }
+            
+        }
+        
+
+        
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e) //PUSH
+        {
+            TextBox tb = new TextBox();
+
+
+            if (st.Count<10)
+            {
+                
+                tb.Visible = true;
+                if (textBox1.Text != "")
+                {
+                    tb.Text = textBox1.Text;
+                }
+                else
+                {
+                    tb.Text = "0";
+                }
+                tb.Left = 15;
+                tb.Top = 0;
+                for (int wys = 15; wys <= tablicaY[st.Count]; wys++)
+                {
+                    tb.Top += 1;
+                    Thread.Sleep(2);
+                    this.panel1.Controls.Add(tb);
+                }
+                st.Push(tb);
+                textBox1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Osiągnięto maksymalną liczbę textboxów!");
+            }
+
+
+            
+           
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e) //POP
+        {
+            if (st.Count > 0)
+            {
+                TextBox wyciagnieta = st.Pop();
+                
+                for(int lewo = 15; lewo <= 220; lewo++)
+                {
+                    wyciagnieta.Left +=1;
+                    Thread.Sleep(2);
+                }
+                MessageBox.Show("Wyciągnięto ze stosu: " + wyciagnieta.Text);
+                this.panel1.Controls.Remove(wyciagnieta);
+            }
+            else
+            {
+                MessageBox.Show("Stos jest pusty!");
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e) //CLEAR
+        {
+            if (st.Count > 0)
+            {
+                for (int j = st.Count; j > 0; j--)
+                {
+
+                    TextBox wyciagnieta = st.Pop();
+                    for (int lewo = 15; lewo <= 220; lewo++)
+                    {
+                        wyciagnieta.Left += 1;
+                        Thread.Sleep(2);
+                    }
+                    this.panel1.Controls.Remove(wyciagnieta);
+                    
+                }
+                MessageBox.Show("Usunięto wszystkie textboxy");
+
+
+            }
+            else
+            {
+                MessageBox.Show("Stos jest pusty!");
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
